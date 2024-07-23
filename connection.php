b301f191525884e4f83bb1187735a965b1e72124
Albulenalala <100690726+Albulenalala@users.noreply.github.com>
2024-07-23 15:07:31 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-    $database= new mysqli("localhost","root","","edoc");
+    $database= new mysqli("localhost","root","","drTech");
     if ($database->connect_error){
         die("Connection failed:  ".$database->connect_error);
     }
