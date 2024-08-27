3d803f15f6c173cf9bae9430d7b7f043f91265b0
Albulenalala <100690726+Albulenalala@users.noreply.github.com>
2024-08-27 13:21:45 +0200

    Update signup.php

--- signup.php
+++ signup.php
@@ -7,88 +7,134 @@
     <link rel="stylesheet" href="css/animations.css">  
     <link rel="stylesheet" href="css/main.css">  
     <link rel="stylesheet" href="css/signup.css">
+        
     <title>Sign Up</title>
+    
 </head>
 <body>
+<?php
+
+//learn from w3schools.com
+//Unset all the server side variables
+
+session_start();
+
+$_SESSION["user"]="";
+$_SESSION["usertype"]="";
+
+// Set the new timezone
+date_default_timezone_set('Asia/Kolkata');
+$date = date('Y-m-d');
+
+$_SESSION["date"]=$date;
+
+
+
+if($_POST){
+
+    
+
+    $_SESSION["personal"]=array(
+        'fname'=>$_POST['fname'],
+        'lname'=>$_POST['lname'],
+        'address'=>$_POST['address'],
+        'nic'=>$_POST['nic'],
+        'dob'=>$_POST['dob']
+    );
+
+
+    print_r($_SESSION["personal"]);
+    header("location: create-account.php");
+
+
+
+
+}
+
+?>
+
+
     <center>
-        <div class="container">
-            <form action="" method="POST">
-                <table border="0">
-                    <tr>
-                        <td colspan="2">
-                            <p class="header-text">Let's Get Started</p>
-                            <p class="sub-text">Add Your Personal Details to Continue</p>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="label-td">
-                            <label for="fname" class="form-label">First Name: </label>
-                            <input type="text" name="fname" class="input-text" placeholder="First Name" required>
-                        </td>
-                        <td class="label-td">
-                            <label for="lname" class="form-label">Last Name: </label>
-                            <input type="text" name="lname" class="input-text" placeholder="Last Name" required>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="label-td" colspan="2">
-                            <label for="address" class="form-label">Address: </label>
-                            <input type="text" name="address" class="input-text" placeholder="Address" required>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="label-td" colspan="2">
-                            <label for="nic" class="form-label">NIC: </label>
-                            <input type="text" name="nic" class="input-text" placeholder="NIC Number" required>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="label-td" colspan="2">
-                            <label for="dob" class="form-label">Date of Birth: </label>
-                            <input type="date" name="dob" class="input-text" required>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <input type="reset" value="Reset" class="login-btn btn-primary-soft btn">
-                        </td>
-                        <td>
-                            <input type="submit" value="Next" class="login-btn btn-primary btn">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colspan="2">
-                            <br>
-                            <label for="" class="sub-text" style="font-weight: 280;">Already have an account? </label>
-                            <a href="login.php" class="hover-link1 non-style-link">Login</a>
-                            <br><br><br>
-                        </td>
-                    </tr>
-                </table>
-            </form>
-        </div>
-    </center>
+    <div class="container">
+        <table border="0">
+            <tr>
+                <td colspan="2">
+                    <p class="header-text">Let's Get Started</p>
+                    <p class="sub-text">Add Your Personal Details to Continue</p>
+                </td>
+            </tr>
+            <tr>
+                <form action="" method="POST" >
+                <td class="label-td" colspan="2">
+                    <label for="name" class="form-label">Name: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td">
+                    <input type="text" name="fname" class="input-text" placeholder="First Name" required>
+                </td>
+                <td class="label-td">
+                    <input type="text" name="lname" class="input-text" placeholder="Last Name" required>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <label for="address" class="form-label">Address: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <input type="text" name="address" class="input-text" placeholder="Address" required>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <label for="nic" class="form-label">NIC: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <input type="text" name="nic" class="input-text" placeholder="NIC Number" required>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <label for="dob" class="form-label">Date of Birth: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <input type="date" name="dob" class="input-text" required>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                </td>
+            </tr>
 
-    <?php
-    session_start();
-    date_default_timezone_set('Asia/Kolkata');
-    $date = date('Y-m-d');
-    $_SESSION["date"] = $date;
+            <tr>
+                <td>
+                    <input type="reset" value="Reset" class="login-btn btn-primary-soft btn" >
+                </td>
+                <td>
+                    <input type="submit" value="Next" class="login-btn btn-primary btn">
+                </td>
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $_SESSION["personal"] = array(
-            'fname' => $_POST['fname'],
-            'lname' => $_POST['lname'],
-            'address' => $_POST['address'],
-            'nic' => $_POST['nic'],
-            'dob' => $_POST['dob']
-        );
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <br>
+                    <label for="" class="sub-text" style="font-weight: 280;">Already have an account&#63; </label>
+                    <a href="login.php" class="hover-link1 non-style-link">Login</a>
+                    <br><br><br>
+                </td>
+            </tr>
 
-        // Redirect to create-account.php after form submission
-        header("location: create-account.php");
-        exit(); // Stop further script execution
-    }
-    ?>
+                    </form>
+            </tr>
+        </table>
 
+    </div>
+</center>
 </body>
 </html>
